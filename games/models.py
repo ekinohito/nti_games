2d8c67a22bd44a4ffcb62c6ce6a6e9635bc9e23b
Platun0v <platun0v@protonmail.com>
2020-12-10 20:32:07 +0300

    Fix for postgres

--- games/models.py
+++ games/models.py
@@ -7,5 +7,5 @@ class TalantUser(models.Model):
     access_token = models.CharField(max_length=2500)
 
     steam_openid = models.CharField(max_length=200, default='')
-    steam_id = models.IntegerField(default=0)
+    steam_id = models.BigIntegerField(default=0)
     # talant
