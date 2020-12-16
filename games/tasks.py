56edf733fb9f09f4569f99ec3460164134369f48
Platun0v <platun0v@protonmail.com>
2020-12-16 22:39:29 +0300

    Add cs support and better dota

--- games/tasks.py
+++ games/tasks.py
@@ -1,6 +1,11 @@
+import json
+
 from celery import shared_task
-from dota.algo_dota import DotaAnalysing
 from django.contrib.auth.models import User
+from django.conf import settings
+
+from dota.algo_dota import DotaAnalysing
+from cs_go.algo_cs_go import  CSGOAnalysing
 
 
 @shared_task
@@ -13,7 +18,7 @@ def dota_count(user_id: int):
     user = User.objects.get(pk=user_id)
     steam_id = user.talantuser.steam_id
 
-    dota = DotaAnalysing(76561198397930966 - steam_id)
+    dota = DotaAnalysing(steam_id)
     try:
         res = dota.start()
     except Exception as e:
@@ -23,8 +28,31 @@ def dota_count(user_id: int):
 
         raise e
 
-    user.talantuser.dota_result = res
+    user.talantuser.dota_result = json.dumps(res)
     user.talantuser.dota_task = ''
     user.talantuser.save()
 
     return res
+
+
+@shared_task
+def cs_count(user_id: int):
+    user = User.objects.get(pk=user_id)
+    steam_id = user.talantuser.steam_id
+
+    cs = CSGOAnalysing(settings.STEAM_API_KEY, steam_id)
+    try:
+        res = cs.start()
+    except Exception as e:
+        user.talantuser.cs_result = None
+        user.talantuser.cs_task = ''
+        user.talantuser.save()
+
+        raise e
+
+    user.talantuser.cs_result = json.dumps(res)
+    user.talantuser.cs_task = ''
+    user.talantuser.save()
+
+    return res
+    pass
